0bdf1331aac65e528a780428e08c55518b6f64ec
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-18 16:04:06 +0200

    Create PostModal.tsx

---
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -0,0 +1,9 @@
+import { type CardProps } from '@/interfaces/index'
+
+const PostModal: React.FC<CardProps> = ({ title, content }) =>{
+    return(
+        <div></div>
+    )
+}
+
+export default PostModal;
