1a48f59c6c9c4375e48ede6b2fbd2dcc1d277fad
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-18 16:17:16 +0200

    Update PostModal.tsx

--- alx-project-0x02/components/common/PostModal.tsx
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -2,11 +2,11 @@ import { type PostModalProps } from '@/interfaces/index'
 
 const PostModal: React.FC<PostModalProps> = ({ title, content }) =>{
     return(
-        <form action="post">
+        <form action="post" className='border border-2 grid grid-col-2'>
             <label htmlFor="">Title</label>
-            <input type="text" />
+            <input type="text" className='border' />
             <label htmlFor="">content</label>
-            <input type="text" />
+            <input type="text" className='border'/>
             <button>add</button>
         </form>
     )
