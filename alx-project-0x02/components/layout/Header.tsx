f037dedeccb51020184d08e13ff6c9c320a2f728
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-22 00:36:03 +0200

    add post to the navigation bar

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,15 +2,21 @@ import Link from "next/link";
 
 const Header: React.FC = () =>{
     return(
-        <header className="border border-[#FF5A5F] rounded-full mx-[500px] mb-[10px] h-[50px]">
+        <header className="border border-[#FF5A5F] rounded-full mx-[500px] mb-[10px] h-[50px] px-10">
             <nav className=" mt-3 text-xl">
-                <ul className="flex flex-wrap items-center justify-center">
+                <ul className="flex flex-wrap items-center justify-between ">
+
                     <li className="border-r border-[#FF5A5F] pr-5">
                         <Link href='/home' className="hover:border-b">
                             🏠Home
                         </Link>
                     </li>
-                    <li className="pl-5">
+                    <li className="border-r border-[#FF5A5F] pr-5">
+                        <Link href='/posts' className="hover:border-b">
+                            🪧Posts
+                        </Link>
+                    </li>
+                    <li className="pr-5">
                         <Link href='/about' className="hover:border-b">
                             ✉️About
                         </Link>
