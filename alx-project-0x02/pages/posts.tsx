59f8f5b2ef10351dd50e8f2838d1dc71aedfcbeb
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-22 12:38:17 +0200

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,44 +1,42 @@
 import { PostProps } from "@/interfaces";
-import Header from '@/components/layout/Header'
-import { useEffect, useState } from "react";
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
-const Post: React.FC<PostProps> = () => {
-    const [posts, setPosts]= useState<PostProps[]>([])
-    useEffect(() => {
-        async function fetchPosts() {
-            const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-            const data: {
-            userId: number;
-            id: number;
-            title: string;
-            body: string;
-            }[] = await res.json();
-
-            const formatted: PostProps[] = data.map((post) => ({
-            id: post.id,
-            title: post.title,
-            content: post.body,
-            userId: post.userId,
-            }));
-
-            setPosts(formatted);
-        }
-
-        fetchPosts();
-        }, []);
-
-    return(
-        <div className="m-10 ">
-            <Header />
-            <div className="p-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                {posts.map((post) => (
-                    <PostCard 
-                    key={post.id} title={post.title} content={post.content} userId={post.userId} id={post.id}/>
-                ))}
-            </div>
-        </div>
-    );
+interface PostsPageProps {
+  posts: PostProps[];
 }
 
-export default Post;
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const posts: PostProps[] = data.map((post: any) => ({
+    id: post.id,
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return { props: { posts } };
+};
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+  return (
+    <div className="m-10">
+      <Header />
+      <div className="p-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {posts.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            content={post.content}
+            userId={post.userId}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
